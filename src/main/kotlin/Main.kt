28f4549514ed60c3d7f02697621e9c0708055d83
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-03-30 19:37:30 +0300

    Homework for lesson 7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
-    println("Hello!")
+    task1()
+    task2()
+    task3()
+}
+
+fun task1() {
+    println("TASK 1:")
+    createThreadsDifferentWays()
+    println()
+}
+
+fun task2() {
+    println("TASK 2:")
+    runProducerAndWatcherThreads()
+    println()
+}
+
+fun task3() {
+    println("TASK 3:")
+    comparePoolsOfDifferentSizes()
+    println()
 }
