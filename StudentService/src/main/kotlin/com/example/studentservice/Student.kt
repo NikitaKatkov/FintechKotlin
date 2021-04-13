4da2fcf1da02d682e31d38af371a3dacd4e3cb79
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-04-13 20:09:03 +0300

    Homework for lesson 9: some documentation via spring fox

--- StudentService/src/main/kotlin/com/example/studentservice/Student.kt
+++ StudentService/src/main/kotlin/com/example/studentservice/Student.kt
@@ -1,8 +1,16 @@
 package com.example.studentservice
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
+@ApiModel
 data class Student(
+    @ApiModelProperty("Unique identifier of a particular student")
     val id: Int,
+    @ApiModelProperty("Name of a particular student")
     val name: String,
+    @ApiModelProperty("Second name of a particular student")
     val secondName: String,
+    @ApiModelProperty("Faculty a particular student studies in")
     val faculty: String = "maths"
 )
--- StudentService/src/main/kotlin/com/example/studentservice/StudentController.kt
+++ StudentService/src/main/kotlin/com/example/studentservice/StudentController.kt
@@ -1,25 +1,38 @@
 package com.example.studentservice
 
 import com.example.studentservice.FacultyServiceInteraction.getFacultyForStudent
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.web.bind.annotation.*
 import kotlin.random.Random
 
 @RestController
 @RequestMapping("/students")
 class StudentController {
+    @ApiOperation("Returns all registered students")
     @GetMapping("/all")
     fun getStudents(): List<Student> {
         println("found all students")
         return listOf(Student(Random.nextInt(), "Nikita", "Katkov"))
     }
 
+    @ApiOperation("Returns registered student with given id if it exists")
+    @ApiResponses(
+        ApiResponse(code = 200, message = "Ok, student exists"),
+        ApiResponse(code = 404, message = "Student with given id doesn't exist")
+    )
     @GetMapping("/{id}")
-    fun getStudentById(@PathVariable id: Int): Student? {
+    fun getStudentById(
+        @ApiParam("id of a particular student to search for") @PathVariable id: Int
+    ): Student {
         println("found student with id: $id")
         val facultyForStudent = getFacultyForStudent(id).orEmpty()
         return Student(id, "Nikita", "Katkov", facultyForStudent)
     }
 
+    @ApiOperation("")
     @PostMapping("/add")
     fun addStudent(): Int {
         val id = Random.nextInt()
@@ -28,12 +41,16 @@ class StudentController {
     }
 
     @PutMapping("/updateSecondName/{newName}")
-    fun updateSecondName(@PathVariable newName: String) {
+    fun updateSecondName(
+        @ApiParam("updated second name of a particular student") @PathVariable newName: String
+    ) {
         println("updated student's second name to $newName")
     }
 
     @DeleteMapping("/delete/{id}")
-    fun deleteById(@PathVariable id: Int) {
+    fun deleteById(
+        @ApiParam("id of a particular student to delete") @PathVariable id: Int
+    ) {
         println("deleted student with id $id")
     }
 }
